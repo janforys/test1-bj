85ab8ab4431ab42f517d806d6100b3cb0c1fe4ef
janforys <janforys@wp.pl>
2023-04-30 13:16:00 +0200

    update

---
+++ helo/Main.java
@@ -0,0 +1,33 @@
+package helo;
+
+class Main
+{
+    /**
+     * Methods that does the work 
+     */
+    public void text()
+    {
+        System.out.println("Hello, KoKo");
+    }
+    
+    public void text2()
+    {
+        System.out.println("Goodbye, KoK0o");
+    }
+    
+    /**
+     *  main method for testing outside BlueJ 
+     */
+    public static void main(String[] args)
+    {
+        Main hi = new Main();
+        int x = 0;
+        
+        while (x <= 4) {
+            hi.text();
+            if (x == 3)
+                hi.text2();
+            x = x + 1;
+        }
+    }
+}
